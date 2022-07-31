49e02e0488e476637ce6eed4b6094690e46e4403
Jason Yang <yangjinlong86@gmail.com>
2022-07-31 16:15:29 +0800

    init project

---
+++ src/main/java/org/nocoder/demo/Test.java
@@ -0,0 +1,12 @@
+package org.nocoder.demo;
+
+/**
+ * @author yangjl
+ * @description test
+ * @date 2022-07-31 15:07
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
